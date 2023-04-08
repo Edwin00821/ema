724de4fc2620ac7eefdfb656154c7824f67deeaa
Edwin00821 <99764706+Edwin00821@users.noreply.github.com>
2023-04-07 20:10:46 -0600

    Update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './Views/**/*.{js,ts,jsx,tsx}',
+    './views/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
